6b651e7066115b29bf0bed8c746afc6718519887
GabeN <gabriele.nardi.dev@gmail.com>
2022-04-22 09:22:25 +0200

    ExampleVC provisional

--- Example/EasyHierarchy/AppDelegate.swift
+++ Example/EasyHierarchy/AppDelegate.swift
@@ -13,11 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow()
+        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        self.window = window
+        self.window?.makeKeyAndVisible()
         return true
     }
 }
--- Example/EasyHierarchy/ViewController.swift
+++ Example/EasyHierarchy/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import EasyHierarchy
 
 class ViewController: UIViewController {
 
+    private let containerVStack = VStack()
+    private let row = HStack()
+    private let label = UILabel(text: "Label", textColor: .black)
+    private let textField = UITextField(textColor: .black, placeholder: "Write something", placeholderColor: .lightGray)
+    private let clearButton = UIButton(title: "Clear", textColor: .blue)
+    
+    override func loadView() {
+        super.loadView()
+        view.addSubview(containerVStack, insets: UIEdgeInsets(top: 8, left: 24, bottom: 16, right: 24))
+        containerVStack.arrange([row, clearButton])
+        row.arrange([label, textField])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        clearButton.addTarget(self, action: #selector(didTapOnClearButton), for: .touchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func didTapOnClearButton() {
+        textField.text = ""
     }
-
 }
-
