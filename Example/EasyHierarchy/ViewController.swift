d0a9ed1910dd442e7f350e19c79746f5cce11adb
GabeN <gabriele.nardi.dev@gmail.com>
2022-05-01 18:18:28 +0200

    Create form as example

--- Example/EasyHierarchy/ViewController.swift
+++ Example/EasyHierarchy/ViewController.swift
@@ -10,27 +10,38 @@ import UIKit
 import EasyHierarchy
 
 class ViewController: UIViewController {
-
-    private let containerVStack = VStack()
-    private let row = HStack()
-    private let label = UILabel(text: "Label", textColor: .black)
-    private let textField = UITextField(textColor: .black, placeholder: "Write something", placeholderColor: .lightGray)
-    private let clearButton = UIButton(title: "Clear", textColor: .blue)
+    private let containerVStack = VStack(spacing: 24)
+    private let surnameLabel = UILabel(text: "Surname", textColor: .black)
+    private let surnameTextField = UITextField(textColor: .black, placeholder: "Doe", placeholderColor: .lightGray)
+    private let nameLabel = UILabel(text: "Name", textColor: .black)
+    private let nameTextField = UITextField(textColor: .black, placeholder: "John", placeholderColor: .lightGray)
+    private let addressLabel = UILabel(text: "Address", textColor: .black)
+    private let addressTextField = UITextField(textColor: .black, placeholder: "5th street", placeholderColor: .lightGray)
+    private let streetNumberLabel = UILabel(text: "Street Number", textColor: .black)
+    private let streetNumberTextField = UITextField(textColor: .black, placeholder: "21", placeholderColor: .lightGray)
+    private let cityLabel = UILabel(text: "City", textColor: .black)
+    private let cityTextField = UITextField(textColor: .black, placeholder: "Cupertino", placeholderColor: .lightGray)
+    private let countryLabel = UILabel(text: "Country", textColor: .black)
+    private let countryTextField = UITextField(textColor: .black, placeholder: "California", placeholderColor: .lightGray)
     
     override func loadView() {
         super.loadView()
         view.addSubview(containerVStack, insets: UIEdgeInsets(top: 8, left: 24, bottom: 16, right: 24))
-        containerVStack.arrange([row, clearButton])
-        row.arrange([label, textField])
+        let labels = [surnameLabel, nameLabel, addressLabel, streetNumberLabel, cityLabel, countryLabel]
+        let textFields = [surnameTextField, nameTextField, addressTextField, streetNumberTextField, cityTextField, countryTextField]
+        containerVStack.arrange(makeRows(labels: labels, textFields: textFields) + [UIView()])
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        clearButton.addTarget(self, action: #selector(didTapOnClearButton), for: .touchUpInside)
     }
     
-    @objc private func didTapOnClearButton() {
-        textField.text = ""
+    private func makeRows(labels: [UILabel], textFields: [UITextField]) -> [UIStackView] {
+        return zip(labels, textFields).map { label, textField in
+            let row = HStack()
+            row.arrange([label, UIView(), textField])
+            return row
+        }
     }
 }
